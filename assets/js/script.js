157f91d12ed13f87f94f0755a2c7994e2d3362b6
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-11 23:16:07 -0500

    added initializers

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// initializers
+var button = document.querySelector("button"); 
+var container = document.querySelector("container");
+var start = document.querySelector("start");
+var quiz = document.querySelector("quiz");
+var timerEl = document.querySelector(".timer");
+var mainEl = document.querySelector("main");
+var counter = local.Storage.getItem("counter");
+
+// if statements
+
+
+// functions
