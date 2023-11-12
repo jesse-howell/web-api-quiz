8388adf6192971618f3fa0c36bd196690cbf1c18
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-11 23:38:53 -0500

    added event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,24 @@ var button = document.querySelector("button");
 var container = document.querySelector("container");
 var start = document.querySelector("start");
 var quiz = document.querySelector("quiz");
-var timerEl = document.querySelector(".timer");
+var timerEl = localStorage.querySelector(".timer");
 var mainEl = document.querySelector("main");
 var counter = local.Storage.getItem("counter");
+var submit = localStorage.getItem("submit")
 
 // if statements
 
 
 // functions
+// added event listeners
+startButton.addEventListener("click" | null, function() {
+
+}
+quiz.Button.addEventListener("click" | null, function() {
+
+}
+submit.button.addEventListener("click" | null, function(){
+
+}
+
+init()
