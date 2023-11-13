d4a51b62d0f457073fb709c4f8c61584dc33e930
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-12 23:39:47 -0500

    added variables and functions for the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,14 @@
 // changed initializer names
 // added startButton and quizButton var
 // removed quizButton var, added answerButton and submitButton vars
+// added question vars
 var startPage = document.querySelector('.start');
 var quizPage = document.querySelector('.quiz');
 var scorePage = document.querySelector('.score');
+var firstQuestion = document.querySelector("#correct");
+var secondQuestion = document.querySelector("#correct");
+var thirdQuestion = document.querySelector("#correct");
+var fourthQuestion = document.querySelector("#correct")
 var startButton = document.querySelector("#start_btn");
 var answerButton = document.querySelector("#correct");
 var submitButton = document.querySelector("#submit");
@@ -27,6 +32,47 @@ function showQuiz() {
     scorePage.style.display = "none";
  
 }
+// added showQuestion functions
+function showfirstQuestion() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    scorePage.style.display = "none";
+    firstQuestion.style.display = null;
+    secondQuestion.style.display = "none";
+    thirdQuestion.style.display = "none";
+    fourthQuestion.style.display = "none";
+}
+
+function showsecondQuestion() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    scorePage.style.display = "none";
+    firstQuestion.style.display = "none";
+    secondQuestion.style.display = null;
+    thirdQuestion.style.display = "none";
+    fourthQuestion.style.display = "none";
+}
+
+function showthirdQuestion() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    scorePage.style.display = "none";
+    firstQuestion.style.display = "none";
+    secondQuestion.style.display = "none";
+    thirdQuestion.style.display = null;
+    fourthQuestion.style.display = "none";
+}
+
+function showfourthQuestion() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    scorePage.style.display = "none";
+    firstQuestion.style.display = "none";
+    secondQuestion.style.display = "none";
+    thirdQuestion.style.display = "none";
+    fourthQuestion.style.display = null;
+}
+
 
 function showScore() {
     startPage.style.display = "none";
@@ -36,8 +82,12 @@ function showScore() {
 }
 // added startButton eventlistener
 // added answer and submit EventListeners
-startButton.addEventListener("click", showQuiz); 
-answerButton.addEventListener("click" , showScore);
+startButton.addEventListener("click" , showQuiz); 
+// firstQuestion.addEventListener("click" , function(event) {
+//     var correctAnswer = "C. Application Programming Interface";
+//     event.correctAnswer.addEventListener(event , correctAnswer);
+// });
+
 submitButton.addEventListener("click" , showStart);
 
 
