e11626983bc012ef0f1d83609c80656b9a8d5290
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-14 00:00:19 -0500

    added div id=question to each question, added data-correct=correct to the correct answers, added class=answer to each correct answer button, changed question var querySelectors to #question, removed showQuestion functions, added quizPage event listener, var answer event.target and if else statements for the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@
 // added startButton and quizButton var
 // removed quizButton var, added answerButton and submitButton vars
 // added question vars
+// changed question var querySelectors to #question
 var startPage = document.querySelector('.start');
 var quizPage = document.querySelector('.quiz');
 var scorePage = document.querySelector('.score');
-var firstQuestion = document.querySelector("#correct");
-var secondQuestion = document.querySelector("#correct");
-var thirdQuestion = document.querySelector("#correct");
-var fourthQuestion = document.querySelector("#correct")
+var firstQuestion = document.querySelector("#question-one");
+var secondQuestion = document.querySelector("#question-two");
+var thirdQuestion = document.querySelector("#question-three");
+var fourthQuestion = document.querySelector("#question-four")
 var startButton = document.querySelector("#start_btn");
 // var answerButton = document.querySelector("#correct");
 var submitButton = document.querySelector("#submit");
@@ -25,53 +26,78 @@ function showStart() {
     scorePage.style.display = "none";
 
 }
-
+// removed showQuestion functions
+// added quizPage event listener, var answer event.target and if else statements for the questions
 function showQuiz() {
     startPage.style.display = "none";
     quizPage.style.display = null;
+    firstQuestion.style.display = "block";
+    secondQuestion.style.display = "none";
+    thirdQuestion.style.display = "none";
+    fourthQuestion.style.display = "none";
     scorePage.style.display = "none";
- 
+    quizPage.addEventListener("click", function(event) {
+     var answer = event.target
+     if (answer.matches(".answer")) {
+        if (firstQuestion.style.display === "block") {
+            firstQuestion.style.display = "none";
+            secondQuestion.style.display = "block";    
+        } else if (secondQuestion.style.display === "block") {
+                    secondQuestion.style.display = "none";
+                    thirdQuestion.style.display = "block";
+                
+        } else if (thirdQuestion.style.display === "block") {
+                    thirdQuestion.style.display = "none";
+                    fourthQuestion.style.display = "block";
+        } else if (fourthQuestion.style.display === "block") {
+                    fourthQuestion.style.display = "none";
+                    showScore();
+        }
+
+     }
+    })
 }
 // added showQuestion functions
-function showfirstQuestion() {
-    startPage.style.display = "none";
-    quizPage.style.display = "none";
-    scorePage.style.display = "none";
-    firstQuestion.style.display = null;
-    secondQuestion.style.display = "none";
-    thirdQuestion.style.display = "none";
-    fourthQuestion.style.display = "none";
-}
+// function showfirstQuestion() {
+//     startPage.style.display = "none";
+//     quizPage.style.display = "none";
+//     scorePage.style.display = "none";
+//     firstQuestion.style.display = null;
+//     secondQuestion.style.display = "none";
+//     thirdQuestion.style.display = "none";
+//     fourthQuestion.style.display = "none";
+    
+// }
 
-function showsecondQuestion() {
-    startPage.style.display = "none";
-    quizPage.style.display = "none";
-    scorePage.style.display = "none";
-    firstQuestion.style.display = "none";
-    secondQuestion.style.display = null;
-    thirdQuestion.style.display = "none";
-    fourthQuestion.style.display = "none";
-}
+// function showsecondQuestion() {
+//     startPage.style.display = "none";
+//     quizPage.style.display = "none";
+//     scorePage.style.display = "none";
+//     firstQuestion.style.display = "none";
+//     secondQuestion.style.display = null;
+//     thirdQuestion.style.display = "none";
+//     fourthQuestion.style.display = "none";
+// }
 
-function showthirdQuestion() {
-    startPage.style.display = "none";
-    quizPage.style.display = "none";
-    scorePage.style.display = "none";
-    firstQuestion.style.display = "none";
-    secondQuestion.style.display = "none";
-    thirdQuestion.style.display = null;
-    fourthQuestion.style.display = "none";
-}
+// function showthirdQuestion() {
+//     startPage.style.display = "none";
+//     quizPage.style.display = "none";
+//     scorePage.style.display = "none";
+//     firstQuestion.style.display = "none";
+//     secondQuestion.style.display = "none";
+//     thirdQuestion.style.display = null;
+//     fourthQuestion.style.display = "none";
+// }
 
-function showfourthQuestion() {
-    startPage.style.display = "none";
-    quizPage.style.display = "none";
-    scorePage.style.display = "none";
-    firstQuestion.style.display = "none";
-    secondQuestion.style.display = "none";
-    thirdQuestion.style.display = "none";
-    fourthQuestion.style.display = null;
-}
+// function showfourthQuestion() {
+//     startPage.style.display = "none";
+//     quizPage.style.display = "none";
+//     scorePage.style.display = "none";
+//     firstQuestion.style.display = "none";
+//     secondQuestion.style.display = "none";
+//     thirdQuestion.style.display = "none";
+//     fourthQuestion.style.display = null;
+// }
 
 
 function showScore() {
@@ -84,9 +110,12 @@ function showScore() {
 // added answer and submit EventListeners
 startButton.addEventListener("click" , showQuiz); 
 
-submitButton.addEventListener("click" , showStart);
+submitButton.addEventListener("click" , showScore);
 
-
+function pagePropagation(event) {
+    event.target.matches("#correct")
+    console.log("MATCH", correct);
+}
 
 
 function init() {
